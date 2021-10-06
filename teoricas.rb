884583f2bacbf738481601c5e6cb1ad3f88a8a3c
Douglas Espindola <despin94@gmail.com>
2021-10-06 09:08:54 -0300

    Concerto de erros semanticos

--- teoricas.rb
+++ teoricas.rb
@@ -1,13 +1,13 @@
 # 3) sobre rotas para mostrar um player (não lembro o controller específico;)
 get 'players/:id' => 'players#show'
 #outra opção seria usar resources
-resources 'players', only [:show]
+resources 'players', only: [:show]
 #dessa forma seria criada apenas a rota /players/:id
 #na questão tinha a seguinte opção que criaria todas as rotas para um CRUD e uma aplicação REST
 resources 'players' #=> que criaria as rotas para os métodos [:index,:show, :update, :create, :destroy]
 
-# 4) Realizazar uma chamada para o banco de dados sem precisar chamar a tabela inteira
-#As duas opções que achei validas seriam:
+# 4) Realizar uma chamada para o banco de dados sem precisar chamar a tabela inteira. Retornar um array de IDs de forma otimizada
+#Duas opções na questão que seriam validas seriam:
 Model.pluck(:id) #=> retornaria uma Array<id>
 #poderiamos usar
 model = Model.select(:id) #=> Retornaria Um objeto de Active Record Collection com [{:id}, {:id}]
